7922948f1c7912728bf2b4ab2b4c0687555f46e8
aibidapo <aibidapo@gmail.com>
2024-03-27 04:20:52 -0400

    create variables and randomness in resources

--- main.tf
+++ main.tf
@@ -1,10 +1,24 @@
+resource "random_id" "random" {
+  byte_length = 2
+}
+
+
 resource "aws_vpc" "ai-devops-prod" {
-  cidr_block           = "10.123.0.0/16"
+  cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
 
   tags = {
-    Name = "ai_devops_prod_vpc"
+    Name = "ai_devops_prod_vpc-${random_id.random.dec}"
 
+  }
+}
+
+
+resource "aws_internet_gateway" "ai-devops-prod-gw" {
+  vpc_id = aws_vpc.ai-devops-prod.id
+
+  tags = {
+    Name = "ai-devops-prod-gw-${random_id.random.dec}"
   }
 }
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "vpc_cidr" {
+  type        = string
+  description = "cidr address of the vpc"
+  default     = "10.123.0.0/16"
+}
